5bbe1f692653b95279fa784a1f3fd718cb1c9849
daiQTB97 <ferodaitranba@gmail.com>
2022-03-09 17:23:05 +0700

    update

---
+++ toanbai7.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputheight;
+inputWidth = prompt("nhap chieu rong");
+inputheight = prompt("nhap chieu cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputheight);
+let area = width*height;
+alert("dien tich hinh chu nhat la= " +area);
